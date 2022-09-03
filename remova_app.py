931adabe587eac321730e2a7137d8bc0019306c2
gxgao <gxgao@andrew.cmu.edu>
2022-09-03 14:36:15 -0400

    Naming changed

--- remova_app.py
+++ remova_app.py
@@ -3,11 +3,11 @@ from PIL import Image
 
 
 st.write("""
-# Fella Remova 
-Get rid of all those pesky fellas!
+# Peopla Remova 
+Reclaim nature! 
 """)
 
-pic = st.file_uploader("Picture to remove dem fellas", 
+pic = st.file_uploader("Picture to remove all the people", 
                 type=["png", "jpg"], accept_multiple_files=False, 
                 key=None, help=None, on_change=None, 
                 args=None, kwargs=None, disabled=False)
@@ -15,11 +15,17 @@ pic = st.file_uploader("Picture to remove dem fellas",
 if pic: 
     image = Image.open(pic)
     ## hook backend here 
-
+  
     st.image(image, caption='Uploaded Image.', use_column_width=True)
 
-    if st.button("Remove da fella's in this image?"):
-        st.write("ok..")
+    if st.button("Remove People in this image?"):
+        # implement back end here
+        # once image is done ask to contribute to the database
+        contributed = False 
+        if st.button("Contribute to the map?", disabled = contributed):
+            contributed = True
+            st.write("Nature is reclaimed!")
+            # add the image to our database
 
 
 else:
