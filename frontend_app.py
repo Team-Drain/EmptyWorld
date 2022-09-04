c560a80a4e1a497c5f326ee0b269a77b42f1b138
gxgao <gxgao@andrew.cmu.edu>
2022-09-03 22:31:05 -0400

    reset state

--- frontend_app.py
+++ frontend_app.py
@@ -14,57 +14,56 @@ st.write("""
 # See what the world looks like without people in the way
 """)
 
-pic = st.file_uploader("Please upload a picture in png or jpg format", 
-                type=["png", "jpg"], accept_multiple_files=False, 
-                key=None, help=None, on_change=None, 
-                args=None, kwargs=None, disabled=False)
-
-
 convertedImage = None 
+pic = None 
 
-if pic: 
-    image = Image.open(pic)
-    ## hook backend here 
-  
-    st.image(image, caption='Uploaded Image.', use_column_width=True)
+while True: 
+    pic = st.file_uploader("Please upload a picture in png or jpg format", 
+                    type=["png", "jpg"], accept_multiple_files=False, 
+                    key=None, help=None, on_change=None, 
+                    args=None, kwargs=None, disabled=False)
 
-    if st.button("Remove People in this image?"):
-        # implement back end here
-        # once image is done ask to contribute to the database
-        convertedImage = do_it(image)
-        print("backend here")
-else:
-    st.write("Please upload an image in a jpeg or png format!") 
-
-if convertedImage != None and st.button("Contribute to the map?"):
-    pic = None 
-    st.write("#Empty worlds!")
-
-    credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"]
-    )
-
-    client = storage.Client(credentials=credentials)
-
-    bucket = client.get_bucket(GLOBAL_BUCKET)
-    
-    imageName =f"{uuid.uuid1()}.jpeg"
-    path = f"{imageName}"
-
-    blob = bucket.blob(f"{imageName}")
-
-    bs = BytesIO()
-    convertedImage.save(bs, "jpeg")
-    blob.upload_from_string(bs.getvalue(), content_type="image/jpeg")
-
-    st.image(convertedImage, caption='Empty World.', use_column_width=True)   
-
-
-if st.button("Reset?"):
-<<<<<<< HEAD
-  
-    raise RerunException
-=======
-    pic = None 
     convertedImage = None 
->>>>>>> e611f3aec92652c6bddca8531ac43214f2883e78
+
+
+    if pic: 
+        image = Image.open(pic)
+        ## hook backend here 
+    
+        st.image(image, caption='Uploaded Image.', use_column_width=True)
+
+        if st.button("Remove People in this image?"):
+            # implement back end here
+            # once image is done ask to contribute to the database
+            convertedImage = do_it(image)
+            print("backend here")
+    else:
+        st.write("Please upload an image in a jpeg or png format!") 
+
+    if convertedImage != None and st.button("Contribute to the map?"):
+        pic = None 
+        st.write("#Empty worlds!")
+
+        credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"]
+        )
+
+        client = storage.Client(credentials=credentials)
+
+        bucket = client.get_bucket(GLOBAL_BUCKET)
+        
+        imageName =f"{uuid.uuid1()}.jpeg"
+        path = f"{imageName}"
+
+        blob = bucket.blob(f"{imageName}")
+
+        bs = BytesIO()
+        convertedImage.save(bs, "jpeg")
+        blob.upload_from_string(bs.getvalue(), content_type="image/jpeg")
+
+        st.image(convertedImage, caption='Empty World.', use_column_width=True)   
+
+
+    if st.button("Reset?"):
+        pic = None 
+        convertedImage = None 
