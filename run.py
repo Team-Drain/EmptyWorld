70cf39c49e16760a8058a2d31faa6dc13999d1eb
shiv-666 <shiv-666@users.noreply.github.com>
2022-09-03 20:16:53 -0400

    reqs

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from models import remove_people, get_human_mask
 from PIL import Image
 
-img = Image.open("IMG_1640.jpeg")
+img = Image.open(r"images\IMG_1640.jpeg")
 mask = get_human_mask(img)
 result = remove_people(img, mask)
 result.save("result.png")
